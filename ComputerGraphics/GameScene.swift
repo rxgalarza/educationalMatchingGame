c80c934707afdefc7d1267066b6a8e22b52f4292
Adetoye <adetoyeefunnuga@yahoo.com.ph>
2019-04-11 15:21:14 -0500

    Added More Animals for Randomization

--- ComputerGraphics/GameScene.swift
+++ ComputerGraphics/GameScene.swift
@@ -17,51 +17,97 @@ class GameScene: SKScene {
     fileprivate func setupAnimals() {
         // code to create a animal sprite
         let hippo: SKSpriteNode = SKSpriteNode(imageNamed: "hippo")
-        hippo.position = CGPoint(x: -100, y: 0)
         hippo.name = "hippo"
         animals.append(hippo)
         
         let panda: SKSpriteNode = SKSpriteNode(imageNamed: "panda")
-        panda.position = CGPoint(x: 100, y: 0)
         panda.name = "panda"
         animals.append(panda)
         
         let horse: SKSpriteNode = SKSpriteNode(imageNamed: "horse")
-        horse.position = CGPoint(x: -100, y: 200)
         horse.name = "horse"
         animals.append(horse)
         
         let chicken: SKSpriteNode = SKSpriteNode(imageNamed: "chicken")
-        chicken.position = CGPoint(x: -100, y: 200)
         chicken.name = "chicken"
         animals.append(chicken)
         
-        
-        // addChild(panda)
         let elephant: SKSpriteNode = SKSpriteNode(imageNamed: "elephant")
-        elephant.position = CGPoint(x: -100, y: -200)
         elephant.name = "elephant"
         animals.append(elephant)
         
         let pig: SKSpriteNode = SKSpriteNode(imageNamed: "pig")
-        pig.position    = CGPoint(x: 100, y: -200)
         pig.name = "pig"
         animals.append(pig)
         
         let dog: SKSpriteNode = SKSpriteNode(imageNamed: "dog")
-        dog.position    = CGPoint(x: -100, y: 100)
         dog.name = "dog"
         animals.append(dog)
         
         let goat: SKSpriteNode = SKSpriteNode(imageNamed: "goat")
-        goat.position    = CGPoint(x: 100, y: 100)
         goat.name = "goat"
         animals.append(goat)
+        
+        let cow: SKSpriteNode = SKSpriteNode(imageNamed: "cow")
+        cow.name = "cow"
+        animals.append(cow)
+        
+        let zebra: SKSpriteNode = SKSpriteNode(imageNamed: "zebra")
+        zebra.name = "zebra"
+        animals.append(zebra)
+        
+        let whale: SKSpriteNode = SKSpriteNode(imageNamed: "whale")
+        whale.name = "whale"
+        animals.append(whale)
+        
+        let rabbit: SKSpriteNode = SKSpriteNode(imageNamed: "rabbit")
+        rabbit.name = "rabbit"
+        animals.append(rabbit)
+        
+        let monkey: SKSpriteNode = SKSpriteNode(imageNamed: "monkey")
+        monkey.name = "monkey"
+        animals.append(monkey)
+        
+        let owl: SKSpriteNode = SKSpriteNode(imageNamed: "owl")
+        owl.name = "owl"
+        animals.append(owl)
+        
+        let frog: SKSpriteNode = SKSpriteNode(imageNamed: "frog")
+        frog.name = "frog"
+        animals.append(frog)
+        
+        let duck: SKSpriteNode = SKSpriteNode(imageNamed: "duck")
+        duck.name = "duck"
+        animals.append(duck)
+        
+        let bear: SKSpriteNode = SKSpriteNode(imageNamed: "bear")
+        bear.name = "bear"
+        animals.append(bear)
+        
+        let parrot: SKSpriteNode = SKSpriteNode(imageNamed: "parrot")
+        parrot.name = "parrot"
+        animals.append(parrot)
+        
+        let penguin: SKSpriteNode = SKSpriteNode(imageNamed: "penguin")
+        penguin.name = "penguin"
+        animals.append(penguin)
+        
+        let giraffe: SKSpriteNode = SKSpriteNode(imageNamed: "giraffe")
+        giraffe.name = "giraffe"
+        animals.append(giraffe)
+        
+        let rhino: SKSpriteNode = SKSpriteNode(imageNamed: "rhino")
+        rhino.name = "rhino"
+        animals.append(rhino)
+        
+        let snake: SKSpriteNode = SKSpriteNode(imageNamed: "snake")
+        snake.name = "snake"
+        animals.append(snake)
     }
     
     fileprivate func setupLabels() {
         let animalToSelectLabel:SKLabelNode = SKLabelNode()
-        animalToSelectLabel.text = "Find the panda"
+        animalToSelectLabel.text = "Find the: "
         animalToSelectLabel.fontName = "AvenirNext-Heavy"
         animalToSelectLabel.fontColor = .black
         animalToSelectLabel.fontSize = 65
@@ -147,6 +193,38 @@ class GameScene: SKScene {
                 print("panda")
             }else if (name == "chicken"){
                 print("chicken")
+            }else if (name == "horse"){
+                    print("horse")
+            }else if (name == "hippo"){
+                print("hippo")
+            }else if (name == "cow"){
+                print("cow")
+            }else if (name == "zebra"){
+                print("zebra")
+            }else if (name == "whale"){
+                print("whale")
+            }else if (name == "rabbit"){
+                print("rabbit")
+            }else if (name == "monkey"){
+                print("monkey")
+            }else if (name == "owl"){
+                print("owl")
+            }else if (name == "frog"){
+                print("frog")
+            }else if (name == "duck"){
+                print("duck")
+            }else if (name == "bear"){
+                print("bear")
+            }else if (name == "parrot"){
+                print("parrot")
+            }else if (name == "penguin"){
+                print("penguin")
+            }else if (name == "giraffe"){
+                print("giraffe")
+            }else if (name == "rhino"){
+                print("rhino")
+            }else if (name == "snake"){
+                print("snake")
             }
             
         }
